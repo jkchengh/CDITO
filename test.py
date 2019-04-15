36375f696a6002ee8c28b6fed89c1df4ccd88a44
Jingkai Chen <jkchen@csail.mit.edu>
2019-04-15 15:52:44 -0400

    CDITO draft

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-# L = [1, 2, 3, 4, 5]
-# phi1 = [(1, 5)]
-# phi2 = [(2, 3)]
-# phi3 = [(2, 4)]
-# phi4 = [(3, 1), (4, 1)]
-# Phi = [phi1, phi2, phi3, phi4]
+from tos import tos
+from cdito import cdito, next_move, phi_conflicts, phi_consistent
+
+
+# O = tos(4)
+# print("\n")
+# for o in O:
+#     print(o)
+
+# next_move([0,1,2,3,4], [[(0,2),(0,3)],[(0,3),(1,4)]],2,2,4)
+# next_move([0,1,2,3,4], [[(0,2),(0,3)],[(0,3),(1,4)]],0,0,4)
+
+# phi_consistent([3,4,1,0,2], [[(4,2),(2,3)],[(0,3),(1,4)]])
+# phi_conflicts([0,1,2,3,4], [[(4,2),(3,2)],[(3,0),(4,1)]])
+
+L = [0, 1, 2, 3, 4]
+phi1 = [(0, 4)]
+phi2 = [(1, 2)]
+phi3 = [(1, 3)]
+phi4 = [(2, 0), (3, 0)]
+phi5 = [(3, 0), (4, 1)]
+phi6 = [(0, 2), (0, 3)]
+Phi = [phi1, phi2, phi3, phi4, phi5, phi6]
+P = [(0, 0, 4)]
+cdito(L, P, Phi)
--- tos.py
+++ tos.py
@@ -1,43 +1,34 @@
-def move(L, i, j):
-    newL = L.copy()
-    p = newL.pop(i)
-    if i < j:
-        newL.insert(j, p)
-    else:
-        newL.insert(j+1, p)
-    return newL
+from utils import move
 
-n = 4
-nminus1 = n-1
-L = list(range(0, n))
-O = [L]
-P = [(0, 0, nminus1)]
-while P != []:
-    print("\n")
-    print("L =", L)
-    print("P =", P)
-    (i, j, l) = P[-1]
-    if j != nminus1:
-        (iplus, jplus)  = (i, j + 1)
-    else:
-        (iplus, jplus) = (i + 1, i + 2)
-    print("(i+, j+): ", "(" + repr(iplus) + " , " + repr(jplus) + ")")
-    if iplus < l:
-        P[-1] = (iplus, jplus, l)
-        L = move(L, iplus, jplus)
-        O.append(L)
-        P.append((0, 0, iplus))
-        print("Move to", L)
-    else:
-        P.pop()
-        if P != []:
-            (ip, jp, lp) = P[-1]
-            L = move(L, jp, ip-1)
-        print("Backtrack to", L, "by taking", "(" + repr(jp) + " ," + repr(ip-1) + ")")
 
-print("\n")
-for o in O:
-    print(o)
+def tos(n):
+    n_minus1 = n - 1
+    L = list(range(0, n))
+    O = [L]
+    P = [(0, 0, n_minus1)]
+    while P:
+        print("\n")
+        print("L =", L)
+        print("P =", P)
+        (i, j, l) = P[-1]
+        if j != n_minus1:
+            (next_i, next_j)  = (i, j + 1)
+        else:
+            (next_i, next_j) = (i + 1, i + 2)
+        print("(i+, j+): ", "(" + repr(next_i) + " , " + repr(next_j) + ")")
+        if next_i < l:
+            P[-1] = (next_i, next_j, l)
+            L = move(L, next_i, next_j)
+            O.append(L)
+            P.append((0, 0, next_i))
+            print("Move to", L)
+        else:
+            P.pop()
+            if P:
+                (parent_i, parent_j, parent_l) = P[-1]
+                L = move(L, parent_j, parent_i - 1)
+                print("Backtrack to", L, "by taking", "(" + repr(parent_j) + " ," + repr(parent_i - 1) + ")")
+    return O
 
 
 
